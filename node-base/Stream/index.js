bb0b1dbc489f8fa453c37736ac3751c61a44d9fe
mengbo-ji <jmb33595@163.com>
2021-06-15 17:25:47 +0800

    feat: path测试

--- node-base/Stream/index.js
+++ node-base/Stream/index.js
@@ -6,4 +6,3 @@ const z = zlib.createGzip();
 const wns = fs.createWriteStream('./test.txt.gz');
 
 rns.pipe(z).pipe(wns);
-
--- node-base/path/index.js
+++ node-base/path/index.js
@@ -1,3 +1,9 @@
 
-
+const path = require('path');
+const fs = require('fs');
 // 文件操作在 Node.js 编程中使用频率很高，路径处理是文件操作的前提，Node.js 通过 path 模块提供了路径处理的基础 API
+const appDirectory = fs.realpathSync(process.cwd());
+console.log(appDirectory);
+
+console.log(path.resolve(appDirectory, 'a/index.js'));
+console.log(path.resolve('/foo/bar', 'tmp/file/'));
