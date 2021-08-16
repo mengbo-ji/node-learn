67055e952a07e82e091ebb5e56d5ad81fa869c66
mengbo-ji <jmb33595@163.com>
2021-08-16 11:30:53 +0800

    feat: puppeteer 截图

--- node-base/Puppeteer/index.js
+++ node-base/Puppeteer/index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://www.baidu.com');
   await page.screenshot({ path: 'example.png' });
 
   await browser.close();
