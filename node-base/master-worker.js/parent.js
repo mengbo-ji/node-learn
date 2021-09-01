c7b69deb6853fa6cfaf03e6b2b53018a6ca7e63b
mengbo-ji <jmb33595@163.com>
2021-09-01 18:25:34 +0800

    feat: master-worker.js

--- node-base/master-worker.js/parent.js
+++ node-base/master-worker.js/parent.js
@@ -9,13 +9,44 @@
 // n.send({ hello: 'world' });
 
 
-const child = require('child_process').fork(__dirname + '/sub.js');
+// const child = require('child_process').fork(__dirname + '/sub.js');
+
+// // Open up the server object and send the handle
+// const server = require('net').createServer();
+// server.on('connection', function(socket) {
+//   socket.end('handled by parent\n');
+// });
+// server.listen(1337, function() {
+//   child.send('server', server);
+// });
+
+
+// // parent.js
+// const cp = require('child_process');
+// const child1 = cp.fork(__dirname + '/sub.js');
+// const child2 = cp.fork(__dirname + '/sub.js');
+
+// // Open up the server object and send the handle
+// const server = require('net').createServer();
+// server.on('connection', function(socket) {
+//   socket.end('handled by parent\n');
+// });
+// server.listen(1337, function() {
+//   child1.send('server', server);
+//   child2.send('server', server);
+// });
+
+
+// parent.js
+const cp = require('child_process');
+const child1 = cp.fork(__dirname + '/sub.js');
+const child2 = cp.fork(__dirname + '/sub.js');
 
 // Open up the server object and send the handle
 const server = require('net').createServer();
-server.on('connection', function(socket) {
-  socket.end('handled by parent\n');
-});
 server.listen(1337, function() {
-  child.send('server', server);
+  child1.send('server', server);
+  child2.send('server', server);
+  // 关掉
+  server.close();
 });
--- node-base/master-worker.js/worker.js
+++ node-base/master-worker.js/worker.js
@@ -1,5 +1,41 @@
+// const http = require('http');
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World\n');
+// }).listen(Math.round((1 + Math.random()) * 1000), '127.0.0.1');
+
+
+// worker.js
 const http = require('http');
-http.createServer(function(req, res) {
+const logger = require('logger').createLogger();
+const server = http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(Math.round((1 + Math.random()) * 1000), '127.0.0.1');
+  res.end('handled by child, pid is ' + process.pid + '\n');
+  throw new Error('throw exception');
+});
+
+let worker;
+process.on('message', function(m, tcp) {
+  if (m === 'server') {
+    worker = tcp;
+    worker.on('connection', function(socket) {
+      server.emit('connection', socket);
+    });
+  }
+});
+
+process.on('uncaughtException', function(err) {
+  // 记录日志
+  logger.error(err);
+  // 发送自杀信号
+  process.send({ act: 'suicide' });
+  // 停止接收新的连接
+  worker.close(function() {
+    // 所有已有连接断开后，退出进程
+    process.exit(1);
+  });
+  // 5秒后退出进程
+  setTimeout(function() {
+    process.exit(1);
+  }, 5000);
+});
