127d11e810aeb5af5fdb2583617a584e2294cc15
吉孟波 <mengbo.ji@tongdun.net>
2021-12-24 18:34:29 +0800

    feat: 文件读取server开发

--- my-server/index.js
+++ my-server/index.js
@@ -1,4 +1,11 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs').promises;
+const url = require('url');
+const mime = require('mime');
+const ejs = require('ejs');
+const { promisify } = require('util');
+const { createReadStream } = require('fs');
 
 function mergeConfig(config) {
   return {
@@ -18,8 +25,47 @@ class Server {
       console.log('服务端启动了......');
     });
   }
-  serverHandler(req, res) {
-    console.log('有请求进来了');
+  async serverHandler(req, res) {
+    let { pathname } = url.parse(req.url);
+    pathname = decodeURIComponent(pathname);
+    const absPath = path.join(this.config.directory, pathname);
+    try {
+      const stat = await fs.stat(absPath);
+      if (stat.isFile()) {
+        this.fileHandler(req, res, absPath);
+      } else {
+        let dirs = await fs.readdir(absPath);
+        dirs = dirs.map(v => {
+          return {
+            path: path.join(pathname, v),
+            name: v,
+          };
+        });
+        const renderFile = promisify(ejs.renderFile);
+        const parentPath = path.dirname(pathname);
+        const ret = await renderFile(path.resolve(__dirname, 'template.html'), {
+          arr: dirs,
+          parent: pathname !== '/',
+          parentPath,
+          title: path.basename(pathname) || 'Index',
+        });
+        res.end(ret);
+      }
+    } catch (error) {
+      this.errorHandler(req, res, error);
+    }
+    console.log(absPath);
+  }
+  errorHandler(req, res, error) {
+    console.log(error);
+    res.statusCode = 404;
+    res.setHeader('Content-type', 'text/html;charset=utf-8');
+    res.end('Not Found');
+  }
+  fileHandler(req, res, absPath) {
+    res.statusCode = 200;
+    res.setHeader('Content-type', `${mime.getType(absPath)};charset=utf-8`);
+    createReadStream(absPath).pipe(res);
   }
 }
 
